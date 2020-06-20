198e775eb789738629c2c806788b63687dfd051c
benzocv <anzarahmedkhan99@gmail.com>
2020-06-20 08:59:34 +0530

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const requestHandler = (req, res) => {
+  //New anonymous function to handl the request and response
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Send Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send Message</button></form></p></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>This is my first page in node</title></head>");
+  res.write("<body><p>Hello people I'm Anzarahmedkhan. </p></body>");
+  res.write("</html>");
+  res.end();
+};
+module.exports = requestHandler;
