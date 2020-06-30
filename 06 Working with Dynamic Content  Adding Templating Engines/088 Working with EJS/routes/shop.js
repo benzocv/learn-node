4667f3164cd55022408411737767ae896144a589
benzocv <grabthestie.itsolution@gmail.com>
2020-06-30 17:49:53 +0530

    ejs added

---
+++ 06 Working with Dynamic Content  Adding Templating Engines/088 Working with EJS/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+const adminData = require("./admin.js");
+
+const router = express.Router();
+
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;  
+  res.render('shop',{prods: products, docTitle : 'Shop',path:'/shop'});
+});
+
+module.exports = router;
