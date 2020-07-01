107cb6c7c1164d005d97325b77f358bf901472a9
benzocv <grabthestie.itsolution@gmail.com>
2020-07-01 12:16:55 +0530

    file save and retrive added

---
+++ 07 The Model View Controller (MVC)/097 Storing Data in Files Via the Model/models/product.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  //to save product
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json "
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      console.log(fileContent);
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  //to fetch product
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json "
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
